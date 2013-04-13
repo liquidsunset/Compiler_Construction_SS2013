40352ad3ffb5aedd9fa6cadf49ae132cb37251a0
Alexander Miller <alles.mil@gmail.com>
2013-04-13 18:15:45 +0200

    merged

--- src/parser.c
+++ src/parser.c
@@ -5,13 +5,20 @@ int EOF;
 EOF = 509;
 
 // Reports errors of parser
-void error()
-{
-	error(0);
-}
 
 void error(int token)
 {
+    
+}
+
+
+int isIn(int tokenType, int rule){
+    
+    if(tokenType == 510 && rule == 0){ return 1;} // preprocessor
+    if((tokenType == 1 || tokenType == 2 || tokenType == 3) && rule == 1){return 1;} // function_definition
+    if(tokenType == 10 && rule == 2){return 1;} //variable_declaration global
+    
+    return 0;
     
 }
 

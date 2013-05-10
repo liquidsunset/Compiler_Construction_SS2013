8aa0de414e78925aae0a412a7e061432c3af6c3b
Alexander Miller <alles.mil@gmail.com>
2013-05-10 15:48:35 +0200

    path relative to project root

--- src/parser.c
+++ src/parser.c
@@ -527,14 +527,14 @@ void start() {
 int main(){
     printf("Phoenix: Parser\n");
     initTokens();
-    openFile("../test/invalid_parser.c");
+    openFile("test/invalid_parser.c");
     //openFile("/Users/liquidsunset/Documents/Angewandte_Informatik/4. Semester/Compilerbau/Phoenix/test/scanner.c");
     start();
     printf("Parsed with %d errors, %d warnings\n", errorCount, warningCount);
     errorCount = 0;
     warningCount = 0;
     tokenType = -1;
-    openFile("../test/valid_parser.c");
+    openFile("test/valid_parser.c");
     //openFile("/Users/liquidsunset/Documents/Angewandte_Informatik/4. Semester/Compilerbau/Phoenix/test/valid_parser.c");
     start();
     printf("Parsed with %d errors, %d warnings\n", errorCount, warningCount);

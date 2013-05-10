eadb3b8c5061259f5ea48ff7b318209785f20b75
Alexander Miller <alles.mil@gmail.com>
2013-05-10 16:56:26 +0200

    testfile for milestone4

---
+++ test/m4.c
@@ -0,0 +1,18 @@
+struct rec_t {
+	    int f;
+	    int g;
+	    int *arr;
+};
+
+void main() {
+	int a;
+	struct rec_t *rec;
+
+	a = 5;
+	rec = malloc(sizeof(struct rec_t));
+	rec->arr = malloc(sizeof(int) * 3);
+	rec->arr[1] = 4;
+	rec->f = 7;
+	rec->g = 9;
+	rec->arr[0] = a + (a - rec->arr[1]) * rec->f - a / rec->g * (12 - 11);
+}

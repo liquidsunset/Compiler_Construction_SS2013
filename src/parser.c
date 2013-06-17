ce570baa69e1cb42cb4acae158fbed77c3feece9
liquidsunset <dbrand@cosy.sbg.ac.at>
2013-06-17 10:32:16 +0200

    dirty fix

--- src/parser.c
+++ src/parser.c
@@ -2672,7 +2672,7 @@ struct object_t * formalParameter(
     {
         if(formalParameter != 0)
         {
-            if(type != formalParameter->type)
+            if(type->form != formalParameter->type->form)
             {
                 mark("type mismatch in procedure declaration and call (formalParameter)");
             }

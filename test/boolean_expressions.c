875be02470fea6db2788c9784f1d0b03db3e1e9f
Alexander Miller <alles.mil@gmail.com>
2013-06-17 15:20:28 +0200

    test file for boolean expressions

---
+++ test/boolean_expressions.c
@@ -0,0 +1,20 @@
+void main()
+{
+	if(1 == 1) { printf("OK: =="); }
+	else { printf("ERROR: =="); }
+
+	if(1 <= 1) { printf("OK: <="); }
+	else { printf("ERROR: <="); }
+
+	if(1 >= 1) { printf("OK: >="); }
+	else { printf("ERROR: >="); }
+
+	if(1 != 1) { printf("ERROR: !="); }
+	else { printf("OK: !="); }
+
+	if(1 < 1) { printf("ERROR: <"); }
+	else { printf("OK: <"); }
+
+	if(1 > 1) { printf("ERROR: >"); }
+	else { printf("OK: >"); }
+}

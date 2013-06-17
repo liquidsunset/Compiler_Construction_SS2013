a891ecb906dc521169613cce7e7e9f0fc3cda130
Alexander Miller <alles.mil@gmail.com>
2013-06-17 14:14:52 +0200

    weird behaviour

--- test/m6a.c
+++ test/m6a.c
@@ -1,17 +1,48 @@
 static int x;
 
-int test(int x, int y)
-{
-	int z;
-	printf(x);
-	printf(y);
-	z = x+y;
-	printf(z);
-	return z;
-}
+int test(int x, int y, int * numbers);
 
 void main()
 {
+	int * numbers;
+	numbers = malloc(sizeof(int) * 3);
+	numbers[0] = 0-5;
+	numbers[1] = 0;
+	numbers[2] = 5;
 	x = 3;
-	x = x + test(7, 5);
+	x = x + test(7, 5, numbers);
+}
+
+int test(int x, int y, int * numbers)
+{
+	int z;
+	int i;
+	i = 0;
+	while(i < 3)
+	{
+		if(numbers[i] >= 0)
+		{
+			if(numbers[i] > 0)
+			{
+				printf(numbers[i]);
+				printf(">0");
+			}
+			else
+			{
+				printf(numbers[i]);
+				printf("=0");
+			}
+		}
+		else
+		{
+			printf(numbers[i]);
+			printf("<0");
+		}
+		i = i + 1;
+	}
+	// printf(numbers[0]);
+	// printf(numbers[1]);
+	// printf(numbers[2]);
+	z = x+y;
+	return z;
 }

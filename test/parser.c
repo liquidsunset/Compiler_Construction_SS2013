52e6e4630d628759bb67bae59dac9ffaafdcc310
liquidsunset <dbrand@cosy.sbg.ac.at>
2013-07-09 19:07:46 +0200

    inittokens in main

--- test/parser.c
+++ test/parser.c
@@ -242,164 +242,8 @@ void getFromList()
 
 //Scanner
 
-void initTokens(){
-    TOKEN_NULL = 0;
-    TOKEN_VOID = 1;
-    TOKEN_INT = 2;
-    TOKEN_CHAR = 3;
-    TOKEN_DOUBLE = 4;
-    TOKEN_WHILE = 5;
-    TOKEN_IF = 6;
-    TOKEN_ELSE = 7;
-    TOKEN_RETURN = 8;
-    TOKEN_STRUCT = 9;
-    TOKEN_STATIC = 10;
-    TOKEN_FOPEN = 11;
-    TOKEN_FCLOSE = 12;
-    TOKEN_FGETC = 13;
-    TOKEN_FPUTC = 14;
-    TOKEN_MALLOC = 15;
-    TOKEN_SIZEOF = 16;
-    TOKEN_TYPEDEF = 17;
-    TOKEN_PRINTF = 18;
-    
-    TOKEN_IDENTIFIER = 100;
-    
-    TOKEN_CONSTINT = 200;
-    TOKEN_CONSTDOUBLE = 201;
-    TOKEN_CONSTCHAR = 202;
-    
-    TOKEN_STRING_LITERAL = 300;
-    
-    TOKEN_PLUS = 400;
-    TOKEN_MINUS = 401;
-    TOKEN_MULT = 402;
-    TOKEN_DIVIDE = 403;
-    TOKEN_PERCENT = 404;
-    TOKEN_ASSIGNMENT = 405;
-    TOKEN_EQUAL = 406;
-    TOKEN_LESSEQUAL = 407;
-    TOKEN_GREATEREQUAL = 408;
-    TOKEN_LESS = 409;
-    TOKEN_GREATER = 410;
-    TOKEN_SHIFTLEFT = 411;
-    TOKEN_SHIFTRIGHT = 412;
-    TOKEN_ADDRESS = 413;
-    TOKEN_AND = 414;
-    TOKEN_BITWISEOR = 415;
-    TOKEN_OR = 416;
-    TOKEN_BITWISEEXCLOR = 417;
-    TOKEN_BITWISENOT = 418;
-    TOKEN_UNEQUAL = 419;
-    TOKEN_INCREMENT = 420;
-    TOKEN_DECREMENT = 421;
-    TOKEN_NOT = 422;
-    TOKEN_ACCESS = 423;
-    TOKEN_BITWISEAND = 424;
-    
-    TOKEN_LSB = 500;
-    TOKEN_RSB = 501;
-    TOKEN_LRB = 502;
-    TOKEN_RRB = 503;
-    TOKEN_LCB = 504;
-    TOKEN_RCB = 505;
-    TOKEN_SEMICOLON = 506;
-    TOKEN_COMMA = 507;
-    TOKEN_COLON = 508;
-    TOKEN_EOF = 509;
-    TOKEN_INCLUDE = 510;
-    TOKEN_POINT = 511;
-    
-    FIRST_EXPRESSION = 0;
-    FIRST_FUNCTION_DEFINITION = 1;
-    FIRST_GLOBAL_VARIABLE_DECLARATION = 2;
-    FIRST_TYPE = 3;
-    FIRST_VARIABLE_DECLARATION = 4;
-    FIRST_FUNCTION_STATEMENT = 5;
-    FIRST_TYPE_DECLARATION = 6;
-    FIRST_INSTRUCTION = 7;
-    FIRST_TOP_DECLARATION = 8;
-    FIRST_BASIC_TYPES = 9;
-    
-    CLASS_FIELD = 0;
-    CLASS_TYPE = 1;
-    CLASS_VAR = 2;
-    CLASS_PROC = 3;
-    CLASS_STRING = 4;
-    
-    FORM_RECORD = 0;
-    FORM_ARRAY = 1;
-    FORM_INT = 2;
-    FORM_CHAR = 3;
-    FORM_BOOL = 4;
-    
-    errorCount = 0;
-    warningCount = 0;
-    
-    isInString = 0;
-    isInChar = 0;
-    
-    currentChar = -1;
-    nextChar = -1;
-    
-    // ---------- Margit ----
-    
-    TARGET_NOP = 0;
-    
-    // F1 (1-23)
-    TARGET_ADDI = 1;
-    TARGET_SUBI = 2;
-    TARGET_MULI = 3;
-    TARGET_DIVI = 4;
-    TARGET_MODI = 5;
-    TARGET_CMPI = 6;
-    TARGET_LW = 7;
-    TARGET_SW = 8;
-    TARGET_POP = 9;
-    TARGET_PSH = 10;
-    TARGET_BEQ = 11;
-    TARGET_BGE = 12;
-    TARGET_BGT = 13;
-    TARGET_BLE = 14;
-    TARGET_BLT = 15;
-    TARGET_BNE = 16;
-    TARGET_BR = 17;
-    TARGET_MALLOC = 19;
-    TARGET_RET = 20;
-    TARGET_FOPEN = 21;
-    TARGET_FGETC = 22;
-    TARGET_FPUTC = 23;
-    
-    // F2 (24-43)
-    
-    TARGET_SUB = 24;
-    TARGET_MUL = 25;
-    TARGET_DIV = 26;
-    TARGET_MOD = 27;
-    TARGET_CMP = 28;
-    TARGET_RET = 29;
-    // TARGET_FLO = 30;
-    // TARGET_FLC = 31;
-    // TARGET_RDC = 32;
-    // TARGET_WRC = 33;
-    TARGET_ANDI = 34;
-    TARGET_OR = 35;
-    TARGET_PRINTF = 36;
-    TARGET_PRINTFI = 37;
-    TARGET_ADD = 38;
-    
-    
-    // F3 (43-63)
-    TARGET_JSR = 43;
-    TARGET_J = 44;
-    TARGET_TRAP = 45;
-    TARGET_FCLOSE = 46;
-    TARGET_BSR = 47;
-    
-    stringValue = malloc(sizeof(char) * 1024);
-    typeName = malloc(sizeof(char) * 1024);
-    isRegisterUsed = malloc(sizeof(int) *32);
-}
+//void initTokens(){
+//}
 
 
 
@@ -4141,7 +3985,165 @@ int main(){
     printf("Phoenix: Parser");
     printf("===============");
 
-    initTokens();
+    //initTokens();
+    
+    TOKEN_NULL = 0;
+    TOKEN_VOID = 1;
+    TOKEN_INT = 2;
+    TOKEN_CHAR = 3;
+    TOKEN_DOUBLE = 4;
+    TOKEN_WHILE = 5;
+    TOKEN_IF = 6;
+    TOKEN_ELSE = 7;
+    TOKEN_RETURN = 8;
+    TOKEN_STRUCT = 9;
+    TOKEN_STATIC = 10;
+    TOKEN_FOPEN = 11;
+    TOKEN_FCLOSE = 12;
+    TOKEN_FGETC = 13;
+    TOKEN_FPUTC = 14;
+    TOKEN_MALLOC = 15;
+    TOKEN_SIZEOF = 16;
+    TOKEN_TYPEDEF = 17;
+    TOKEN_PRINTF = 18;
+    
+    TOKEN_IDENTIFIER = 100;
+    
+    TOKEN_CONSTINT = 200;
+    TOKEN_CONSTDOUBLE = 201;
+    TOKEN_CONSTCHAR = 202;
+    
+    TOKEN_STRING_LITERAL = 300;
+    
+    TOKEN_PLUS = 400;
+    TOKEN_MINUS = 401;
+    TOKEN_MULT = 402;
+    TOKEN_DIVIDE = 403;
+    TOKEN_PERCENT = 404;
+    TOKEN_ASSIGNMENT = 405;
+    TOKEN_EQUAL = 406;
+    TOKEN_LESSEQUAL = 407;
+    TOKEN_GREATEREQUAL = 408;
+    TOKEN_LESS = 409;
+    TOKEN_GREATER = 410;
+    TOKEN_SHIFTLEFT = 411;
+    TOKEN_SHIFTRIGHT = 412;
+    TOKEN_ADDRESS = 413;
+    TOKEN_AND = 414;
+    TOKEN_BITWISEOR = 415;
+    TOKEN_OR = 416;
+    TOKEN_BITWISEEXCLOR = 417;
+    TOKEN_BITWISENOT = 418;
+    TOKEN_UNEQUAL = 419;
+    TOKEN_INCREMENT = 420;
+    TOKEN_DECREMENT = 421;
+    TOKEN_NOT = 422;
+    TOKEN_ACCESS = 423;
+    TOKEN_BITWISEAND = 424;
+    
+    TOKEN_LSB = 500;
+    TOKEN_RSB = 501;
+    TOKEN_LRB = 502;
+    TOKEN_RRB = 503;
+    TOKEN_LCB = 504;
+    TOKEN_RCB = 505;
+    TOKEN_SEMICOLON = 506;
+    TOKEN_COMMA = 507;
+    TOKEN_COLON = 508;
+    TOKEN_EOF = 509;
+    TOKEN_INCLUDE = 510;
+    TOKEN_POINT = 511;
+    
+    FIRST_EXPRESSION = 0;
+    FIRST_FUNCTION_DEFINITION = 1;
+    FIRST_GLOBAL_VARIABLE_DECLARATION = 2;
+    FIRST_TYPE = 3;
+    FIRST_VARIABLE_DECLARATION = 4;
+    FIRST_FUNCTION_STATEMENT = 5;
+    FIRST_TYPE_DECLARATION = 6;
+    FIRST_INSTRUCTION = 7;
+    FIRST_TOP_DECLARATION = 8;
+    FIRST_BASIC_TYPES = 9;
+    
+    CLASS_FIELD = 0;
+    CLASS_TYPE = 1;
+    CLASS_VAR = 2;
+    CLASS_PROC = 3;
+    CLASS_STRING = 4;
+    
+    FORM_RECORD = 0;
+    FORM_ARRAY = 1;
+    FORM_INT = 2;
+    FORM_CHAR = 3;
+    FORM_BOOL = 4;
+    
+    errorCount = 0;
+    warningCount = 0;
+    
+    isInString = 0;
+    isInChar = 0;
+    
+    currentChar = -1;
+    nextChar = -1;
+    
+    // ---------- Margit ----
+    
+    TARGET_NOP = 0;
+    
+    // F1 (1-23)
+    TARGET_ADDI = 1;
+    TARGET_SUBI = 2;
+    TARGET_MULI = 3;
+    TARGET_DIVI = 4;
+    TARGET_MODI = 5;
+    TARGET_CMPI = 6;
+    TARGET_LW = 7;
+    TARGET_SW = 8;
+    TARGET_POP = 9;
+    TARGET_PSH = 10;
+    TARGET_BEQ = 11;
+    TARGET_BGE = 12;
+    TARGET_BGT = 13;
+    TARGET_BLE = 14;
+    TARGET_BLT = 15;
+    TARGET_BNE = 16;
+    TARGET_BR = 17;
+    TARGET_MALLOC = 19;
+    TARGET_RET = 20;
+    TARGET_FOPEN = 21;
+    TARGET_FGETC = 22;
+    TARGET_FPUTC = 23;
+    
+    // F2 (24-43)
+    
+    TARGET_SUB = 24;
+    TARGET_MUL = 25;
+    TARGET_DIV = 26;
+    TARGET_MOD = 27;
+    TARGET_CMP = 28;
+    TARGET_RET = 29;
+    // TARGET_FLO = 30;
+    // TARGET_FLC = 31;
+    // TARGET_RDC = 32;
+    // TARGET_WRC = 33;
+    TARGET_ANDI = 34;
+    TARGET_OR = 35;
+    TARGET_PRINTF = 36;
+    TARGET_PRINTFI = 37;
+    TARGET_ADD = 38;
+    
+    
+    // F3 (43-63)
+    TARGET_JSR = 43;
+    TARGET_J = 44;
+    TARGET_TRAP = 45;
+    TARGET_FCLOSE = 46;
+    TARGET_BSR = 47;
+    
+    stringValue = malloc(sizeof(char) * 1024);
+    typeName = malloc(sizeof(char) * 1024);
+    isRegisterUsed = malloc(sizeof(int) *32);
+
 
     initTypes();
     initCodeGen();
